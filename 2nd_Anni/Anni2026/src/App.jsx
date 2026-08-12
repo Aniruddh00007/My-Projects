cf02a0904b014f75ca0753512625f480be5a0176
Aniruddh Kumar <Aniiiii0007@gmail.com>
2026-08-12 12:35:47 +0530

    new task

---
+++ 2nd_Anni/Anni2026/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <h1 className="text-5xl font-bold text-pink-600 bg-">
+        Happy 2nd Anniversary ❤️
+      </h1>
+    </>
+  )
+}
+
+export default App
